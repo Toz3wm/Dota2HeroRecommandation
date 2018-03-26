576441687c79bb0310e2ad7a7cf5cb2e13e6aca1
Toz3wm <trubenach@airware.com>
2018-03-26 21:27:11 +0200

    data retriever works

--- test1.py
+++ test1.py
@@ -1,9 +1,10 @@
 import traceback
-
+from sys import getsizeof
 import dota2api
 import numpy as np
 import time
 import pickle
+import json
 from dota2api.src.exceptions import APIError
 
 api_file = "cle_api.txt"
@@ -13,44 +14,107 @@ with open(api_file, "r") as f:
     api_key = f.readline()
 
 print("api initialized")
-api = dota2api.Initialise(api_key)
+api = dota2api.Initialise(api_key, logging=False)
 
 
-print('get match 3562464902 details')
-match = api.get_match_details(match_id=3562464902)
+# print('get match 3562464902 details')
+# match = api.get_match_details(match_id=3562464902)
 
-# print(heroes)
-time.sleep(1)
-hist = api.get_match_history(account_id=120062110)
-time.sleep(1)
-hist5 = api.get_match_history(matches_requested='1000')
+# l =  3801842558
+# match = api.get_match_details(match_id=3801842558)
+# print(match)
 
-print(match['radiant_win'])
-print(len(hist))
+# print('==========')
+# hist = api.get_match_history(account_id=120062110)
+# print(match['radiant_win'])
+# print(len(hist))
 
-# hist3 = api.get_match_history_by_seq_num(start_at_match_id='3562464902')
-# hist4 = api.get_match_history_by_seq_num()
 
+# hist3 = api.get_match_history_by_seq_num(start_at_match_seq_num=3562464902)
+# hist4 = api.get_match_history_by_seq_num(start_at_match_seq_num=3791610951)
+# hist5 = api.get_match_history_by_seq_num(start_at_match_seq_num=3301896950)
+# print(list(hist5))
+# exit()
+# print(type(hist5))
+# print('=============')
+# print(list(hist5['matches']))
+# print('==== ==== === ==')
+# ll = list(hist5['matches'])
+# print(ll[2])
+# print(len(ll))
+# hist5 = api.get_match_history_by_seq_num(start_at_match_seq_num=3791610751)
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+    print("saved to " + filename)
+
+
+init_seq_num = 3301896950
+nb_of_hundreds = 50
+
+def createMatchList(init_seq_num, nb_of_hundreds, verbose=True):
+    current_seq_num = init_seq_num
+    res = []
+    for hundred in range(nb_of_hundreds):
+        print('=========================')
+        print(f'{hundred} hundreds out of {nb_of_hundreds}')
+        try:
+            time.sleep(1)
+            hist = api.get_match_history_by_seq_num(start_at_match_seq_num=current_seq_num)
+            status = hist['status']
+            if int(status) == 1:
+                l = list(hist['matches'])
+                if verbose:
+                    for m in l:
+                        match_id = m['match_id']
+                        match_seq_num = m['match_seq_num']
+                        duration = m['duration']
+                        print(f'match id : {match_id}, match seq num : {match_seq_num}, duration : {duration}')
+                res += l
+                s = getsizeof(res)
+                print(f'Size : {s} bytes')
+                current_seq_num = l[-1]['match_seq_num'] + 1
+
+            else:
+                current_seq_num += 100
+                print(f'something went wrong ({status})')
+        except json.decoder.JSONDecodeError:
+            print("something went horribly wrong")
+            traceback.print_exc()
+            current_seq_num = res[-1]['match_seq_num'] + 101
+    return res, current_seq_num
+
+batch, current_seq_num = createMatchList(init_seq_num, nb_of_hundreds, False)
+
+filename = f'seq_start_{init_seq_num}_seq_end_{current_seq_num}_nbhundreds{nb_of_hundreds}.pkl'
+save_object(batch, filename)
 
 
 
 def print_list_match(hist):
     list_of_match = list(hist['matches'])
     for m in list_of_match:
-        id_ = m['match_id']
-        seq_num = m['match_id']
+        print('====================')
+        print(m)
+        match_seq_num = m['match_seq_num']
+        match_id = m['match_id']
         if 'duration' in m:
             duration = m['duration']
         else:
             duration = 'Not available'
-        print(f'match id : {id_}, match seq num : {seq_num}, duration : {duration}')
+        print(f'match id : {match_id}, match seq num : {match_seq_num}, duration : {duration}')
     print("Total number of matches : ",len(list_of_match))
 
 
 # print_list_match(hist)
-# print_list_match(hist5)
 # print_list_match(hist3)
+# print('\n'*5)
 # print_list_match(hist4)
+print('\n'*5)
+print_list_match(hist5)
+
+exit()
 
 def getValidMatchListId(hist):
     r = []
@@ -72,9 +136,6 @@ def heroVectfromMatch(match):
 v = heroVectfromMatch(match)
 
 
-def save_object(obj, filename):
-    with open(filename, 'wb') as output:
-        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
 
 # charge les détails des matches à rebours de starting Index
@@ -84,7 +145,11 @@ def loading_match_routine(index_list, filename):
         try:
             m = api.get_match_details(index)
             l.append(m)
-            print(str(len(l)) + " games downloaded (" + str(index) + ")")
+            if 'duration' in m:
+                duration = m['duration']
+            else:
+                duration = 'Not available'
+            print(str(len(l)) + " games downloaded (" + str(index) + ", duration {})".format(duration))
         except APIError:
             pass
             # traceback.print_exc()
@@ -94,7 +159,7 @@ def loading_match_routine(index_list, filename):
         except Error:
             pass
 
-        time.sleep(1.2)
+        time.sleep(0.01)
         
     print('saving file...')
     t = time.strftime("%d_%b_%H:%M", time.gmtime())
